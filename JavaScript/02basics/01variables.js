d9d7250a510cb8f6a886af33a9f02898a5325daf
Vamshi Muluguri <63087230+vamshi726@users.noreply.github.com>
2021-06-08 21:42:47 +0530

    Add files via upload

---
+++ JavaScript/02basics/01variables.js
@@ -0,0 +1,26 @@
+var fullName = "Vamshi";
+
+//fullName -> camelcase for variables
+//full_name -> snakecase
+//FullName -> for class objects
+
+var courseName = "JS Bootcamp";
+
+var loggedCount = 34
+// console.log(loggedCount);
+// console.log(loggedCount);
+
+var isLoggedIn = false;
+
+var paymentmode;
+
+paymentMode = "Debit card";
+
+console.log(paymentMode);
+
+console.log(typeof(isLoggedIn));
+
+console.log(typeof(loggedCount));
+
+console.log(typeof(courseName));
+
