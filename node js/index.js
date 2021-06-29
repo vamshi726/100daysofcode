654141144bcfd6cc965e1f47084900122ad1cdab
Vamshi Muluguri <63087230+vamshi726@users.noreply.github.com>
2021-06-29 20:24:47 +0530

    Add files via upload

---
+++ node js/index.js
@@ -0,0 +1,14 @@
+
+var http = require('http');
+ 
+// Create a server object:
+http.createServer(function (req, res) {
+ 
+    // Write a response to the client
+    res.write('welcome to node js');
+ 
+    // End the response 
+    res.end();
+ 
+// The server object listens on port 8080
+}).listen(8080);
