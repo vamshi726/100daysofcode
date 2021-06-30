b0f640819fe590a8e433a484568306f7de6e9783
Vamshi Muluguri <63087230+vamshi726@users.noreply.github.com>
2021-06-30 19:28:40 +0530

    Add files via upload

---
+++ node js/intro-to-nodejs/index.js
@@ -0,0 +1,22 @@
+
+
+var superheroes = require("superheroes");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+var supervillains = require("supervillains");
+
+var myvillains = supervillains.random();
+
+console.log(myvillains);
+
+/*
+    output : 
+    user@user-PC MINGW64 /e/vamshi/WEB DEV PROGRAMS/node js
+    $ node index.js 
+    Karma
+    Gorilla Grodd
+
+*/
