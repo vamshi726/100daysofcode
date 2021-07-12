e1dcbf069b42366cb3e0534391bb021336691c18
Vamshi Muluguri <63087230+vamshi726@users.noreply.github.com>
2021-07-12 15:00:59 +0530

    Add files via upload

---
+++ JavaScript/09Advancejs/010functionclos.js
@@ -0,0 +1,15 @@
+// Initiate counter
+let counter = 0;
+
+// Function to increment counter
+function add() {
+  let counter = 0;
+  counter += 1;
+}
+
+// Call add() 3 times
+add();
+add();
+add();
+
+//The counter should now be 3. But it is 0
